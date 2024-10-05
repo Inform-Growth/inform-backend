8fadfc4ec2d84083d83d0431bd5cdead1ebf160d
jaronsander <jar.sand.on@gmail.com>
2024-10-05 12:51:03 -0700

    refactoring

--- app/models/chat_models.py
+++
@@ -1,7 +0,0 @@
-from langchain_core.pydantic_v1 import BaseModel, Field
-from typing import List
-
-
-class QuestionModel(BaseModel):
-    question:str =  Field(..., description="The next form question")
-    suggestions:List[str] = Field(..., description="Predicted answers for the next question")
